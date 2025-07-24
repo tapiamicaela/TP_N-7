5e2773e1f2d89cd8c278078bf8dfeca0955999bc
tapiamicaela <tapiamikaa521@gmail.com>
2025-07-24 18:54:15 -0300

    realizacion del ejercicio 9 y 10 de importacion

---
+++ eje9y10.py
@@ -0,0 +1,19 @@
+"""9) Restaurante importado: Escriba un programa que esté en otro archivo que la clase 
+Restaurante del ejercicio 4, e impórtela al módulo actual. Cree una instancia de Restaurante 
+y llame a alguno de sus métodos para asegurarse que la importación funcionó."""
+
+"""from TP_N7 import Restaurante
+
+restaurante1= Restaurante("Mc Donal's", "comida rapida")
+restaurante1.describir_restaurante()
+restaurante1.abrir_restaurante()"""
+
+"""10) (Opcional): Repita el ejercicio anterior pero esta vez importando la clase Heladeria. ¿Qué se necesita para que funcione la
+importación?"""
+
+"""from TP_N7 import Heladeria
+
+heladeria2= Heladeria("Grido", "Helados", ["Vainilla", "Frutilla a la Crema", "Granizado", "Menta"])
+heladeria2.describir_restaurante()
+heladeria2.abrir_restaurante()
+heladeria2.mostrar_sabores()"""
